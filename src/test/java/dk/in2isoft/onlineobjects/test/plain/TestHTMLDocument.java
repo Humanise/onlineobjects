e42b2a7341f7b4b11d7e3dbc0158e50f978708d5
Jonas Munk <jonas@humanise.dk>
2018-03-22 16:48:52 +0100

    Test of enormous HTML docs

--- src/test/java/dk/in2isoft/onlineobjects/test/plain/TestHTMLDocument.java
+++ src/test/java/dk/in2isoft/onlineobjects/test/plain/TestHTMLDocument.java
@@ -7,25 +7,31 @@ import java.io.FileFilter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.MalformedURLException;
-
-import nu.xom.Document;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.time.StopWatch;
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import dk.in2isoft.commons.lang.Strings;
 import dk.in2isoft.commons.parsing.HTMLDocument;
 import dk.in2isoft.commons.xml.DocumentCleaner;
+import dk.in2isoft.onlineobjects.modules.information.ContentExtractor;
 import dk.in2isoft.onlineobjects.modules.information.SimpleContentExtractor;
 import dk.in2isoft.onlineobjects.modules.networking.HTMLService;
 import dk.in2isoft.onlineobjects.services.SemanticService;
 import dk.in2isoft.onlineobjects.test.AbstractSpringTestCase;
+import nu.xom.Document;
+import nu.xom.Node;
 
 public class TestHTMLDocument extends AbstractSpringTestCase {
 	
-	//private static Logger log = LoggerFactory.getLogger(TestHTMLDocument.class);
+	private static Logger log = LoggerFactory.getLogger(TestHTMLDocument.class);
 	
 	@Autowired
 	private HTMLService htmlService;
@@ -42,6 +48,73 @@ public class TestHTMLDocument extends AbstractSpringTestCase {
 		assertEquals(15402,words.length);
 	}
 	
+	@Test
+	public void testTravel() throws MalformedURLException, IOException {
+		StopWatch watch = new StopWatch();
+		watch.start();
+		watch.split();
+		HTMLDocument doc = htmlService.getDocumentSilently(getTestFile("html/HTML_Standard.html").toURI());
+		watch.split();
+		log.info("Loaded: " + watch.getSplitTime());
+		assertEquals("HTML Standard", doc.getTitle());
+
+		Document xom = doc.getXOMDocument();
+		watch.split();
+		log.info("Get XOMDocument: " + watch.getSplitTime());
+		
+		travel(xom, node -> {
+			//log.info(node.getClass().getSimpleName());
+		});
+		log.info("Finished traveling: " + watch.getSplitTime());
+	}
+	
+	private void travel(Node node, Consumer<Node> consumer) {
+		int count = node.getChildCount();
+		for (int i = 0; i < count; i++) {
+			Node child = node.getChild(i);
+			consumer.accept(child);
+			travel(child, consumer);
+		}
+	}
+
+	@Test
+	public void testEnormousPage() throws MalformedURLException, IOException {
+		StopWatch watch = new StopWatch();
+		watch.start();
+		watch.split();
+		String path = "html/HTML_Standard.html";
+		path = "html/ww11.apirocks.com.html";
+		HTMLDocument doc = htmlService.getDocumentSilently(getTestFile(path).toURI());
+		watch.split();
+		log.info("Loaded: " + watch.getSplitTime());
+		//assertEquals("HTML Standard", doc.getTitle());
+
+		Document xom = doc.getXOMDocument();
+		watch.split();
+		log.info("Get XOMDocument: " + watch.getSplitTime());
+
+		String text = doc.getFullText();
+		watch.split();
+		log.info("Get full text: " + watch.getSplitTime());
+
+		String[] words = semanticService.getWords(text);
+		//assertEquals(513110,words.length);
+		watch.split();
+		log.info("Get words: " + watch.getSplitTime());
+
+
+		ContentExtractor extractor = new SimpleContentExtractor();
+		Document extracted = extractor.extract(xom);
+		watch.split();
+		log.info("Extracted: " + watch.getSplitTime());
+
+		DocumentCleaner cleaner = new DocumentCleaner();
+		cleaner.setUrl(doc.getOriginalUrl());
+		cleaner.clean(extracted);
+		watch.split();
+		log.info("Cleaned: " + watch.getSplitTime());
+	}
+
 	@Test
 	public void testArticle() throws Exception {
 		HTMLDocument doc = htmlService.getDocumentSilently(getTestFile("article.html").toURI());
