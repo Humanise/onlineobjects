095dd7a5e22c7e5a843677a34a9605ff7e688a65
Jonas Munk <jonas@humanise.dk>
2018-06-06 21:09:22 +0200

    Cleaned ud grunt and npm

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-  
+
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -60,56 +60,15 @@ module.exports = function(grunt) {
           ext: '.css'
         }]
       }
-    },
-    qunit: {
-      all: ['test/phantom/*.html']
-    },
-    jsdoc : {
-      dist : {
-        src: ['js/*.js'],
-        options: {
-          destination: 'doc'
-        }
-      }
-    },
-    compass: {
-      full: {
-        options: {
-          sassDir: "scss",
-          cssDir: "css",
-          noLineComments: true,
-        }
-      }
-    },
-    shell: {
-      all : {
-        command : 'tools/all.sh'
-      }
     }
   });
 
   // Load plugins.
-  //grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-symlink');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-jsdoc');
-  grunt.loadNpmTasks('grunt-autoprefixer');
 
   // Default task(s).
-  grunt.registerTask('default', 'Watch', ['watch']);
+  grunt.registerTask('default', 'Watch', ['sass','watch']);
 
-  //grunt.registerTask('test', ['qunit']);
-  grunt.registerTask('test', 'Run tests', function(testname) {
-    if(!!testname) {
-      grunt.config('qunit.all', ['test/phantom/' + testname + '.html']);
-    }
-    grunt.task.run('qunit:all');
-  });
 };
