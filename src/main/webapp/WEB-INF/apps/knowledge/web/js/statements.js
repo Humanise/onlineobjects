562626d0a1b40c5addb3b9ec27c3aebb9c6770a9
Jonas Munk <jonas@humanise.dk>
2018-10-05 16:20:25 +0200

    Improved JS formatting

--- src/main/webapp/WEB-INF/apps/knowledge/web/js/statements.js
+++ src/main/webapp/WEB-INF/apps/knowledge/web/js/statements.js
@@ -1,18 +1,18 @@
 var statementController = {
   name : 'statementEditor',
 
-	window : null,
-	form : null,
+  window : null,
+  form : null,
 
-	$ready : function() {
-		this.window = hui.ui.get('statementWindow');
-		this.form = hui.ui.get('statementForm');
-	},
-	
-	edit : function(id) {
-		this.window.show();
-		this.window.setBusy(true);
-		this._reset();
+  $ready : function() {
+    this.window = hui.ui.get('statementWindow');
+    this.form = hui.ui.get('statementForm');
+  },
+
+  edit : function(id) {
+    this.window.show();
+    this.window.setBusy(true);
+    this._reset();
     hui.ui.request({
       url : '/loadStatement',
       parameters : {id:id},
@@ -23,73 +23,73 @@ var statementController = {
         this.window.setBusy(false);
       }.bind(this)
     });
-	},
-	_reset : function() {
-		this.form.reset();
-		this.object = null;
-	},
-	_setObject : function(object) {
-		this.object = object;
-		this.form.setValues({
-			text: object.text,
+  },
+  _reset : function() {
+    this.form.reset();
+    this.object = null;
+  },
+  _setObject : function(object) {
+    this.object = object;
+    this.form.setValues({
+      text: object.text,
       authors: object.authors,
       questions: object.questions,
       supports: object.supports,
       contradicts: object.contradicts
-		});
-	},
-	$submit$statementForm : function() {
-		var form = hui.ui.get('statementForm');
+    });
+  },
+  $submit$statementForm : function() {
+    var form = hui.ui.get('statementForm');
     var values = this.form.getValues();
-		var data = {
-			id : this.object.id,
-			text : values.text,
+    var data = {
+      id : this.object.id,
+      text : values.text,
       authors : values.authors,
       questions : values.questions,
       supports : values.supports,
       contradicts : values.contradicts
-		};
+    };
     this.window.setBusy(true);
     hui.ui.request({
       url : '/saveStatement',
       json : {data:data},
       $success : function() {
         this._reset();
-				this.window.hide();
-				this._notify();
+        this.window.hide();
+        this._notify();
       }.bind(this),
       $finally : function() {
         this.window.setBusy(false);
       }.bind(this)
     });
-	},
-	$click$deleteStatement : function() {
+  },
+  $click$deleteStatement : function() {
     this.window.setBusy(true);
     hui.ui.request({
       url : '/deleteStatement',
       parameters : {id : this.object.id},
       $success : function(obj) {
         this._reset();
-				this.window.hide();
-				this._notify();
+        this.window.hide();
+        this._notify();
       }.bind(this),
       $finally : function() {
         this.window.setBusy(false);
       }.bind(this)
     });
-	},
-	$userClosedWindow$statementWindow : function() {
-		this._reset();
-	},
+  },
+  $userClosedWindow$statementWindow : function() {
+    this._reset();
+  },
   $render$statementAuthor : function(item) {
     return {
       icon: 'common/person',
       text: item.name || item.title || item.text
     }
   },
-	_notify : function() {
+  _notify : function() {
     hui.ui.callDelegates(this,'statementChanged');
-	}
+  }
 }
 
 hui.ui.listen(statementController);
--- src/main/webapp/WEB-INF/apps/photos/web/js/photo_view.js
+++ src/main/webapp/WEB-INF/apps/photos/web/js/photo_view.js
@@ -1,189 +1,189 @@
 var photoView = {
-	imageId : null,
-	editable : false,
-	username : null,
+  imageId : null,
+  editable : false,
+  username : null,
 
-	$ready : function() {
+  $ready : function() {
     var data = hui.get.firstByClass(document.body, 'js-data');
 
-		photoView.editable = data.getAttribute('data-editable') == 'true';
-		photoView.imageId = parseInt(data.getAttribute('data-id'),10);
-		photoView.username = data.getAttribute('data-username');
+    photoView.editable = data.getAttribute('data-editable') == 'true';
+    photoView.imageId = parseInt(data.getAttribute('data-id'),10);
+    photoView.username = data.getAttribute('data-username');
 
-		if (this.editable) {
-			new oo.InlineEditor({
-				element : 'editableTitle',
-				name : 'titleEditor'
-			});
-			this._addWordBehavior();
-		}
-	},
-	_addWordBehavior : function() {
-		hui.listen(hui.get('words'),'click',this._onClickWord.bind(this))
-	},
-	$valueChanged$titleEditor : function(value) {
-		hui.ui.request({
-			message : {start:'Updating title', delay:300, success:'The title is changed'},
-			url : '/updateTitle',
-			parameters : {id:this.imageId,title:value},
-			$failure : function() {
-				hui.ui.showMessage({text:'Unable to update tile',icon:'common/warning',duration:2000});
-			}
-		})
-	},
-	_onClickWord : function(e) {
-		e = hui.event(e);
-		var a = e.findByTag('a');
-		if (a) {
-			hui.ui.confirmOverlay({element:a,text:'Delete word?',$ok : function() {
-				this._removeWord(parseInt(a.getAttribute('data'),10));
-			}.bind(this)})
-		}
-	},
-	$click$addLocation : function(widget) {
-		var panel = hui.ui.get('locationPanel');
-		panel.position(widget);
-		panel.show()
-	},
-	$click$saveLocation : function() {
-		var values = hui.ui.get('locationForm').getValues(),
-			panel = hui.ui.get('locationPanel');
+    if (this.editable) {
+      new oo.InlineEditor({
+        element : 'editableTitle',
+        name : 'titleEditor'
+      });
+      this._addWordBehavior();
+    }
+  },
+  _addWordBehavior : function() {
+    hui.listen(hui.get('words'),'click',this._onClickWord.bind(this))
+  },
+  $valueChanged$titleEditor : function(value) {
+    hui.ui.request({
+      message : {start:'Updating title', delay:300, success:'The title is changed'},
+      url : '/updateTitle',
+      parameters : {id:this.imageId,title:value},
+      $failure : function() {
+        hui.ui.showMessage({text:'Unable to update tile',icon:'common/warning',duration:2000});
+      }
+    })
+  },
+  _onClickWord : function(e) {
+    e = hui.event(e);
+    var a = e.findByTag('a');
+    if (a) {
+      hui.ui.confirmOverlay({element:a,text:'Delete word?',$ok : function() {
+        this._removeWord(parseInt(a.getAttribute('data'),10));
+      }.bind(this)})
+    }
+  },
+  $click$addLocation : function(widget) {
+    var panel = hui.ui.get('locationPanel');
+    panel.position(widget);
+    panel.show()
+  },
+  $click$saveLocation : function() {
+    var values = hui.ui.get('locationForm').getValues(),
+      panel = hui.ui.get('locationPanel');
 
-		hui.ui.request({
-			message : {start:'Updating location', delay:300, success:'The location is changed'},
-			url : '/updateLocation',
-			json : {id : this.imageId, location : values.location},
-			$success : function() {
-				oo.render({id:'properties'})
-			},
-			$failure : function() {
-				hui.ui.showMessage({text:'Unable to update location',icon:'common/warning',duration:2000});
-			}
-		});
-		panel.hide();
-	},
-	$click$addDescription : function(button) {
-		hui.ui.get('descriptionPages').next()
-		hui.ui.get('description').focus();
-	},
-	$click$cancelDescription : function(button) {
-		hui.ui.get('descriptionPages').next()
-	},
-	$click$saveDescription : function(button) {
-		var text = hui.ui.get('description').getValue();
-		hui.ui.request({
-			message : {start:'Updating description',delay:300, success:'The description is changed'},
-			url : '/updateDescription',
-			parameters : {id : this.imageId, description : text},
-			$success : function() {
-				oo.update({id:'photos_photo_description'});
-			},
-			$failure : function() {
-				hui.ui.showMessage({text:'Unable to update description',icon:'common/warning',duration:2000});
-			}
-		})
-	},
-	$click$syncMetaData : function(button) {
+    hui.ui.request({
+      message : {start:'Updating location', delay:300, success:'The location is changed'},
+      url : '/updateLocation',
+      json : {id : this.imageId, location : values.location},
+      $success : function() {
+        oo.render({id:'properties'})
+      },
+      $failure : function() {
+        hui.ui.showMessage({text:'Unable to update location',icon:'common/warning',duration:2000});
+      }
+    });
+    panel.hide();
+  },
+  $click$addDescription : function(button) {
+    hui.ui.get('descriptionPages').next()
+    hui.ui.get('description').focus();
+  },
+  $click$cancelDescription : function(button) {
+    hui.ui.get('descriptionPages').next()
+  },
+  $click$saveDescription : function(button) {
+    var text = hui.ui.get('description').getValue();
+    hui.ui.request({
+      message : {start:'Updating description',delay:300, success:'The description is changed'},
+      url : '/updateDescription',
+      parameters : {id : this.imageId, description : text},
+      $success : function() {
+        oo.update({id:'photos_photo_description'});
+      },
+      $failure : function() {
+        hui.ui.showMessage({text:'Unable to update description',icon:'common/warning',duration:2000});
+      }
+    })
+  },
+  $click$syncMetaData : function(button) {
     button.setEnabled(false)
-		hui.ui.request({
-			message : {start:'Synchronizing',delay:300, success:'The meta data is synchronized'},
-			url : '/synchronizeMetaData',
-			parameters : {imageId : this.imageId},
-			$success : function() {
-				oo.update({id:'properties'});
-			},
-			$failure : function() {
-				hui.ui.msg({text:'Unable to synchronize metadata',icon:'common/warning',duration:2000});
-			},
+    hui.ui.request({
+      message : {start:'Synchronizing',delay:300, success:'The meta data is synchronized'},
+      url : '/synchronizeMetaData',
+      parameters : {imageId : this.imageId},
+      $success : function() {
+        oo.update({id:'properties'});
+      },
+      $failure : function() {
+        hui.ui.msg({text:'Unable to synchronize metadata',icon:'common/warning',duration:2000});
+      },
       $finally : function() {
         button.setEnabled(true)
       }
-		})
+    })
 
-	},
+  },
 
-	$add$words : function(info) {
-		if (!this.imageId) {
-			throw 'No id';
-		}
-		hui.ui.request({
-			message : {start:'Adding word', delay:300, success:'The word is added'},
-			url : '/relateWord',
-			parameters : {image : this.imageId, word : info.id},
-			$success : info.callback,
-			$failure : function() {
-				hui.ui.showMessage({text:'Unable to add word',icon:'common/warning',duration:2000});
-				info.callback();
-			}
-		})
-	},
+  $add$words : function(info) {
+    if (!this.imageId) {
+      throw 'No id';
+    }
+    hui.ui.request({
+      message : {start:'Adding word', delay:300, success:'The word is added'},
+      url : '/relateWord',
+      parameters : {image : this.imageId, word : info.id},
+      $success : info.callback,
+      $failure : function() {
+        hui.ui.showMessage({text:'Unable to add word',icon:'common/warning',duration:2000});
+        info.callback();
+      }
+    })
+  },
 
-	$delete$words : function(info) {
-		hui.ui.request({
-			message : {start:'Removing word', delay:300, success:'The word is removed'},
-			url : '/removeWord',
-			parameters : {image : this.imageId, word : info.id},
-			$success : info.callback,
-			$failure : function() {
-				hui.ui.showMessage({text:'Unable to remove word',icon:'common/warning',duration:2000});
-				info.callback();
-			}
-		})
-	},
+  $delete$words : function(info) {
+    hui.ui.request({
+      message : {start:'Removing word', delay:300, success:'The word is removed'},
+      url : '/removeWord',
+      parameters : {image : this.imageId, word : info.id},
+      $success : info.callback,
+      $failure : function() {
+        hui.ui.showMessage({text:'Unable to remove word',icon:'common/warning',duration:2000});
+        info.callback();
+      }
+    })
+  },
 
-	$valueChanged$publicAccess : function(value) {
-		hui.ui.request({
-			message : {start:'Changing access', delay:300, success:'Access has changed'},
-			url : '/changeAccess',
-			parameters : {image : this.imageId, 'public' : value},
-			$failure : function() {
-				hui.ui.showMessage({text:'Unable to change access',icon:'common/warning',duration:2000});
-			}
-		})
-	},
+  $valueChanged$publicAccess : function(value) {
+    hui.ui.request({
+      message : {start:'Changing access', delay:300, success:'Access has changed'},
+      url : '/changeAccess',
+      parameters : {image : this.imageId, 'public' : value},
+      $failure : function() {
+        hui.ui.showMessage({text:'Unable to change access',icon:'common/warning',duration:2000});
+      }
+    })
+  },
 
-	$valueChanged$theMap : function(info) {
-		hui.ui.request({
-			message : {start:'Changing location', delay:300, success:'The location is changed'},
-			url : '/updateLocation',
-			json : {id : this.imageId, location : info.location},
-			$success : info.callback,
-			$failure : function() {
-				hui.ui.showMessage({text:'Unable to change location',icon:'common/warning',duration:2000});
-				info.callback();
-			}
-		});
-	},
+  $valueChanged$theMap : function(info) {
+    hui.ui.request({
+      message : {start:'Changing location', delay:300, success:'The location is changed'},
+      url : '/updateLocation',
+      json : {id : this.imageId, location : info.location},
+      $success : info.callback,
+      $failure : function() {
+        hui.ui.showMessage({text:'Unable to change location',icon:'common/warning',duration:2000});
+        info.callback();
+      }
+    });
+  },
 
-	$click$deletePhoto : function(info) {
-		hui.ui.confirmOverlay({widget:info,text:'Delete?',$ok : function() {
-			hui.ui.request({
-				message : {start:'Deleting photo', delay:300, success:'The image is deleted'},
-				url : '/deleteImage',
-				parameters : {imageId : this.imageId},
-				$failure : function() {
-					hui.ui.showMessage({text:'Unable to delete photo',icon:'common/warning',duration:2000});
-				},
-				$success : function() {
-					document.location = '/'+oo.language+'/users/'+this.username+'/'
-				}.bind(this)
-			})
-		}.bind(this)})
-	},
+  $click$deletePhoto : function(info) {
+    hui.ui.confirmOverlay({widget:info,text:'Delete?',$ok : function() {
+      hui.ui.request({
+        message : {start:'Deleting photo', delay:300, success:'The image is deleted'},
+        url : '/deleteImage',
+        parameters : {imageId : this.imageId},
+        $failure : function() {
+          hui.ui.showMessage({text:'Unable to delete photo',icon:'common/warning',duration:2000});
+        },
+        $success : function() {
+          document.location = '/'+oo.language+'/users/'+this.username+'/'
+        }.bind(this)
+      })
+    }.bind(this)})
+  },
 
-	$click$viewMetaData : function() {
-		hui.ui.request({
-			url : '/getMetaData',
-			parameters : {imageId : this.imageId},
-			$object : function(data) {
-				var html = '';
-				hui.each(data,function(key,value) {
-					html+='<p class="photos_photo_property"><strong>' + key + ':</strong> ' + value + '</p>';
-				})
-				hui.build('div',{html:html,parent:hui.get('properties')});
-			}
-		});
-	}
+  $click$viewMetaData : function() {
+    hui.ui.request({
+      url : '/getMetaData',
+      parameters : {imageId : this.imageId},
+      $object : function(data) {
+        var html = '';
+        hui.each(data,function(key,value) {
+          html+='<p class="photos_photo_property"><strong>' + key + ':</strong> ' + value + '</p>';
+        })
+        hui.build('div',{html:html,parent:hui.get('properties')});
+      }
+    });
+  }
 };
 
 hui.ui.listen(photoView);
