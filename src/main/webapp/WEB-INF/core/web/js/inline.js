e04009ece3c13a94107494c66dc690ea83311796
Jonas Munk <jonas@humanise.dk>
2018-10-06 11:22:36 +0200

    Log columns in client side errors

--- src/main/webapp/WEB-INF/core/web/js/inline.js
+++ src/main/webapp/WEB-INF/core/web/js/inline.js
@@ -1,10 +1,10 @@
 hui={_:[],on:function(){this._.push(arguments)}};
 
-window.onerror = function(msg, url, line) {
+window.onerror = function(msg, url, line, column) {
   try {
     var e = encodeURIComponent;
     var img = document.createElement('img');
-    img.src = '/service/dependency/error?message=' + e(msg) +'&file=' + e(url) + '&line=' + e(line) + '&url=' + e(document.location.href)
+    img.src = '/service/dependency/error?message=' + e(msg) +'&file=' + e(url) + '&line=' + e(line) + '&column=' + e(column) + '&url=' + e(document.location.href)
     img.style = 'width:1px;height:1px;position:absolute;';
     document.body.appendChild(img);
   } catch (i) {console.error(i)}
