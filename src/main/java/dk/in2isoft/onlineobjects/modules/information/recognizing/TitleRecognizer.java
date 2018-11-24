b30278dac9fcd56e2c924683c839319d143274e8
Jonas Munk <jonas@humanise.dk>
2018-11-24 11:31:08 +0100

    Use Levenstein to find best title

--- src/main/java/dk/in2isoft/onlineobjects/modules/information/recognizing/TitleRecognizer.java
+++ src/main/java/dk/in2isoft/onlineobjects/modules/information/recognizing/TitleRecognizer.java
@@ -1,5 +1,6 @@
 package dk.in2isoft.onlineobjects.modules.information.recognizing;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.stream.Collectors;
 
 import dk.in2isoft.commons.lang.Strings;
 import dk.in2isoft.commons.xml.DOM;
+import info.debatty.java.stringsimilarity.NormalizedLevenshtein;
 import nu.xom.Document;
 import nu.xom.Element;
 
@@ -31,9 +33,11 @@ public class TitleRecognizer implements Recognizer {
 	public Map<Element,Double> recognize(Document document) {
 		String title = getMainTitle(document);
 		String openGraphTitle = getOpenGraphTitle(document);
+		String twitterTitle = getTwitterTitle(document);
 		Set<String> titles = new HashSet<>();
-		if (Strings.isNotBlank(title)) titles.add(title.toLowerCase());
-		if (Strings.isNotBlank(openGraphTitle)) titles.add(openGraphTitle.toLowerCase().trim());
+		if (Strings.isNotBlank(title)) titles.add(normalize(title));
+		if (Strings.isNotBlank(openGraphTitle)) titles.add(normalize(openGraphTitle));
+		if (Strings.isNotBlank(twitterTitle)) titles.add(normalize(twitterTitle));
 		Map<Element,Double> nodes = new HashMap<>();
 		
 		List<Element> candidates = DOM.findElements(document, element -> {
@@ -46,7 +50,7 @@ public class TitleRecognizer implements Recognizer {
 			if (element.getChildElements().size() == 0) {
 				String text = DOM.getText(element);
 				if (Strings.isNotBlank(text)) {
-					if (titles.contains(text.toLowerCase().trim())) {
+					if (titles.contains(normalize(text))) {
 						return true;
 					}
 				}
@@ -66,21 +70,49 @@ public class TitleRecognizer implements Recognizer {
 		return nodes;
 	}
 	
+	private String normalize(String str) {
+		return str.toLowerCase().replaceAll("\\W", "");
+	}
+	
 	private Element findMain(List<Element> candidates, Set<String> titles) {
 		if (candidates.isEmpty()) return null;
 		if (!titles.isEmpty()) {
-			List<Element> list = candidates.stream().filter(el -> titles.contains(DOM.getText(el).toLowerCase())).collect(Collectors.toList());
+			List<Element> list = candidates.stream().filter(el -> titles.contains(normalize(DOM.getText(el)))).collect(Collectors.toList());
 			if (!list.isEmpty()) {
 				return list.get(0);
 			}
 		}
 		
-		candidates.sort((a,b) -> {
-			int lengthA = DOM.getText(a).length();
-			int lengthB = DOM.getText(b).length();
-			return lengthB - lengthA;
+		List<Candidate> cs = make(candidates);
+		compare(cs, titles);
+		
+		cs.sort((a,b) -> {
+			return b.comparison.compareTo(a.comparison);
 		});
-		return candidates.get(0);
+		return cs.get(0).element;
+	}
+
+	private void compare(List<Candidate> cs, Set<String> titles) {
+		NormalizedLevenshtein l = new NormalizedLevenshtein();
+		for (Candidate candidate : cs) {
+			double comparison = 0;
+			for (String title : titles) {
+				comparison = Math.max(comparison, 1 - l.distance(title, candidate.text));
+			}
+			candidate.comparison = comparison;
+		}
+		
+	}
+
+	private List<Candidate> make(List<Element> candidates) {
+		List<Candidate> out = new ArrayList<>();
+		for (Element element : candidates) {
+			Candidate candidate = new Candidate();
+			candidate.element = element;
+			candidate.text = normalize(DOM.getText(element));
+			out.add(candidate);
+		}
+		return out;
 	}
 
 	private String getMainTitle(Document doc) {
@@ -91,5 +123,16 @@ public class TitleRecognizer implements Recognizer {
 	private String getOpenGraphTitle(Document doc) {
 		Element title = DOM.findElement(doc, element -> DOM.isAny(element, "meta") && DOM.hasAttribute(element,"property","og:title"));
 		return title!=null ? title.getAttributeValue("content") : null;
+	}
+
+	private String getTwitterTitle(Document doc) {
+		Element title = DOM.findElement(doc, element -> DOM.isAny(element, "meta") && DOM.hasAttribute(element,"name","twitter:title"));
+		return title!=null ? title.getAttributeValue("content") : null;
+	}
+
+	private class Candidate {
+		Element element;
+		String text;
+		Double comparison = 0.0;
 	}
 }
--- src/test/java/dk/in2isoft/onlineobjects/test/plain/TestTitleRecognizer.java
+++ src/test/java/dk/in2isoft/onlineobjects/test/plain/TestTitleRecognizer.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import dk.in2isoft.commons.xml.DOM;
 import dk.in2isoft.onlineobjects.modules.information.recognizing.TitleRecognizer;
+import info.debatty.java.stringsimilarity.NormalizedLevenshtein;
 import junit.framework.TestCase;
 import nu.xom.Document;
 import nu.xom.Element;
@@ -15,8 +16,24 @@ import nu.xom.Element;
 public class TestTitleRecognizer extends TestCase {
 
 	@Test
+	public void testComparison() {
+		NormalizedLevenshtein l = new NormalizedLevenshtein();
+		String title = "Apple designer Jony Ive explains how ‘teetering towards the absurd‘ helped him make the iPhone | The Independent";
+		String rightCandidate = "Apple designer Jony Ive explains how ‘teetering towards the absurd’ helped him make the iPhone";
+		String wrongCandidate = "Shape Created with Sketch. Apple designer Jony Ive explains his 'utterly absurd' way of working";
+		
+		double rightDistance = l.distance(title, rightCandidate);
+		double wrongDistance = l.distance(title, wrongCandidate);
+		Assert.assertTrue(wrongDistance > rightDistance);
+		
+	}
+	
+	@Test
 	public void testSimple() {
-		String xml = "<?xml version='1.0'?><html><head></head><body><h1>My title</h1></body></html>";
+		String xml = "<?xml version='1.0'?><html>"
+				+ "<head></head>"
+				+ "<body><h1>My title</h1></body>"
+				+ "</html>";
 		Document document = DOM.parseXOM(xml);
 		Assert.assertNotNull(document);
 
@@ -31,7 +48,9 @@ public class TestTitleRecognizer extends TestCase {
 
 	@Test
 	public void testNonH1() {
-		String xml = "<?xml version='1.0'?><html><head><title>My article</title></head><body><h1>Website</h1><p>My article</p></body></html>";
+		String xml = "<?xml version='1.0'?>"
+				+ "<html><head><title>My article</title></head>"
+				+ "<body><h1>Website</h1><p>My article</p></body></html>";
 		Document document = DOM.parseXOM(xml);
 		Assert.assertNotNull(document);
 
@@ -50,4 +69,55 @@ public class TestTitleRecognizer extends TestCase {
 		}
 	}
 
+	@Test
+	public void testIndependent() {
+		String xml = "<?xml version='1.0'?>"
+				+ "<html><head>"
+				+ "<title>Apple designer Jony Ive explains how &#x2018;teetering towards the absurd&#x2019; helped him make the iPhone | The Independent</title>"
+				+ "<meta name=\"twitter:title\" content=\"Apple designer Jony Ive has explained how &#x2018;teetering towards the absurd&#x2019; helped him make the iPhone\"/></head>"
+				+ "<body><div class=\"header\">"
+				+ "<h1><span>Apple designer Jony Ive explains how ‘teetering towards the absurd’ helped him make the iPhone</span></h1>" 
+				+ "</div><h1 data-title=\"1.0\">"
+				+ "<title>Shape</title> Created with Sketch. Apple designer Jony Ive explains his 'utterly absurd' way of working" 
+				+ "</h1></body></html>";
+		Document document = DOM.parseXOM(xml);
+		Assert.assertNotNull(document);
+
+		TitleRecognizer recognizer = new TitleRecognizer();
+		Map<Element, Double> recognize = recognizer.recognize(document);
+		
+		Assert.assertEquals(1, recognize.size());
+		
+		for (Entry<Element, Double> entry : recognize.entrySet()) {
+			if (entry.getValue() == 1) {
+				Assert.assertEquals("<h1><span>Apple designer Jony Ive explains how ‘teetering towards the absurd’ helped him make the iPhone</span></h1>", entry.getKey().toXML());
+			}
+		}
+	}
+	
+	@Test
+	public void testNorman() {
+		String xml = "<?xml version='1.0'?>"
+				+ "<html><head>"
+				+ "<title>10 Heuristics for User Interface Design: Article by Jakob Nielsen</title>"
+				+ "<meta property=\"og:title\" content=\"10 Heuristics for User Interface Design: Article by Jakob Nielsen\" />" 
+				+ "</head>"
+				+ "<body>"
+				+ "<h1>Browse by Topic and Author</h1>"
+				+ "<h1>10 Usability Heuristics for User Interface Design</h1>"
+				+ "</body></html>";
+		Document document = DOM.parseXOM(xml);
+		Assert.assertNotNull(document);
+
+		TitleRecognizer recognizer = new TitleRecognizer();
+		Map<Element, Double> recognize = recognizer.recognize(document);
+		
+		//Assert.assertEquals(1, recognize.size());
+		
+		for (Entry<Element, Double> entry : recognize.entrySet()) {
+			if (entry.getValue() == 1) {
+				Assert.assertEquals("<h1>10 Usability Heuristics for User Interface Design</h1>", entry.getKey().toXML());
+			}
+		}
+	}
 }
