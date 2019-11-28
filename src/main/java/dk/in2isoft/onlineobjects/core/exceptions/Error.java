56ce185cfc9b31d60b640236a0d980404a1712fb
Jonas Munk <jonas@humanise.dk>
2019-11-28 07:20:27 +0100

    Better password recovery

--- src/main/java/dk/in2isoft/onlineobjects/core/exceptions/Error.java
+++ src/main/java/dk/in2isoft/onlineobjects/core/exceptions/Error.java
@@ -1,5 +1,7 @@
 package dk.in2isoft.onlineobjects.core.exceptions;
 
 public enum Error {
-	emailExists, invalidEmail, emailSameAsCurrent, userExists, noEmail, invalidPassword, noUsername, invalidUsername, noPassword, missingCurrentPassword, missingNewPassword, incorrectCurrentPassword, invalidNewPassword, unableToSendMails, userNotFound, userNotCurrent
+	emailExists, invalidEmail, emailSameAsCurrent, userExists, noEmail, invalidPassword, noUsername, invalidUsername,
+	noPassword, missingCurrentPassword, missingNewPassword, incorrectCurrentPassword, invalidNewPassword,
+	unableToSendMails, userNotFound, userNotCurrent, invalidUsernameOrEmail
 }
