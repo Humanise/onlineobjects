4a5c92fec7ef350be0b5a0553dff3e4df918e094
Jonas Munk <jonas@humanise.dk>
2022-09-09 07:56:44 +0200

    Add basic project class

---
+++ src/main/java/dk/in2isoft/onlineobjects/model/Project.java
@@ -0,0 +1,18 @@
+package dk.in2isoft.onlineobjects.model;
+
+import dk.in2isoft.in2igui.Icons;
+import dk.in2isoft.onlineobjects.model.annotations.Appearance;
+
+@Appearance(icon=Icons.COMMON_FOLDER)
+public class Project extends Entity {
+
+	public static String TYPE = Entity.TYPE+"/Project";
+		
+	public Project() {
+		super();
+	}
+
+	public String getType() {
+		return TYPE;
+	}
+}
